b994075ebae8b35d51bf1b94f006df837b11ffeb
Dani Mijl van der <32570@ma-web.nl>
2023-04-15 21:25:03 +0200

    Half werkende miselijk makende code

---
+++ js/main.js
@@ -0,0 +1,110 @@
+class DataLoader {
+    constructor(url) {
+      this.url = url;
+    }
+  
+    loadData() {
+      fetch(this.url)
+        .then(response => response.json())
+        .then(data => console.log(data.episodes))
+        .catch(error => console.error(error));
+    }
+  }
+  
+  const dataLoader = new DataLoader('../data/data.json');
+  dataLoader.loadData();
+
+
+class Header {
+    headerElement;
+    ILogoelement;
+    h2LogoTextElement
+    placeToRenderHeader;
+
+    constructor(placeToRenderHeader) {
+        this.placeToRenderHeader = document.getElementsByTagName(placeToRenderHeader)[0];
+        this.headerElement = document.createElement("header");
+        this.headerElement.classList = "header";
+
+        this.ILogoelement = document.createElement("i");
+        this.ILogoelement.classList = "fa-solid fa-blog header__logo";
+
+        this.h2LogoTextElement = document.createElement("h2");
+        this.h2LogoTextElement.classList = "header__logo";
+        this.h2LogoTextElement.innerText = "Logo Text";
+    }
+
+    render() {
+        this.headerElement.appendChild(this.ILogoelement);
+        this.headerElement.appendChild(this.h2LogoTextElement);
+        this.placeToRenderHeader.appendChild(this.headerElement);
+    }
+}
+
+class MainPanel {
+    sectionElement;
+    LeftArticleElement;
+    LeftSectionCardElement;
+
+    placeToRenderMainSection;
+    
+  
+    constructor(placeToRenderMainSection) {
+      this.placeToRenderMainSection = document.getElementsByTagName(
+        placeToRenderMainSection
+      )[0];
+      this.sectionElement = document.createElement("section");
+      this.sectionElement.classList = "mainSection";
+
+      this.LeftArticleElement = document.createElement("article");
+      this.LeftArticleElement.classList = "mainSection__left";
+
+      this.LeftSectionCardElement = document.createElement("article");
+      this.LeftSectionCardElement.classList = "mainSection__left__cards";
+    }
+  
+    render() {
+      this.placeToRenderMainSection.appendChild(this.sectionElement);
+      this.sectionElement.appendChild(this.LeftArticleElement);
+      this.LeftArticleElement.appendChild(this.LeftSectionCardElement);
+    }
+  }
+
+class Footer{
+    footerElement;
+    h4TextElement;
+    placeToRenderFooter;
+
+    constructor(placeToRenderFooter){
+        this.placeToRenderFooter = document.getElementsByTagName(placeToRenderFooter)[0];
+        this.footerElement = document.createElement("footer");
+        this.footerElement.classList = "footer";
+
+        this.h4TextElement = document.createElement("h4");
+        this.h4TextElement.innerText = "Gemaakt door - Dani SD2D MediaCollege";
+    }
+
+    render() {
+        this.footerElement.appendChild(this.h4TextElement);
+        this.placeToRenderFooter.appendChild(this.footerElement);
+    }
+}
+
+
+class App {
+    Header;
+    Footer;
+    MainPanel;
+
+    constructor() {
+        this.header = new Header("main");
+        this.footer = new Footer("main");
+        this.MainPanel = new MainPanel("main");
+        
+        this.header.render();
+        this.MainPanel.render();
+        this.footer.render();            
+    }
+}
+
+const app = new App();
